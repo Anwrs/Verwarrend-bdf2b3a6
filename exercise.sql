70c7b0f6d3d94e80c4a5c50cac1de046b4dcf0a0
Anouar ElGhazaoui <2080049@talnet.nl>
2020-03-05 14:26:02 +0100

    done

--- exercise.sql
+++ exercise.sql
@@ -1,10 +1,6 @@
-ALTER TABLE planeten ADD id 
-INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-
-INSERT INTO planeten(naam,diameter,afstand_tot_zon,massa,bezoek_datum,id)
-VALUES
-("Mars", 6794, 227936640, 0.1,NOW(),6);
-
+UPDATE planeten 
+SET naam = "Teenalp"
+WHERE id = 6;
 
 
 
